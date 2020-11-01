07408b37e744fb738fd14a6d5bd9474c53e726d4
Lucas Magalhaes <lucas.magalhaes1985@gmail.com>
2020-10-31 23:39:03 -0300

    Access and download images on Telegram server

---
+++ src/telegram to storage/main.py
@@ -0,0 +1,48 @@
+import requests
+import json
+import sys
+
+# import pdb
+# pdb.set_trace()
+
+def main(request):
+    request_json = ""
+    config = None
+    if "-local" in sys.argv:
+        request_json = json.loads(open("../debug/telegram_to_storage_req.json").read())
+        config = json.loads(open("../bot_config.json").read())
+    else:
+        request_json = request.get_json()
+
+        # Get bot config data
+        config = json.loads(open("bot_config.json").read())
+    
+    token = config["bot_token"]
+
+    for file_id in request_json["file_ids"]:
+
+        try:
+            # Configure get file path and make a request for complete info
+            info = requests.get(config["getFilePath_url"].replace("<token>",token).replace("<file_id>", file_id))
+            info = info.json()["result"]
+            print(info)
+        except:
+            print("Failed to get file info from Telegram")
+
+        if "file_path" in info:
+            try:
+                # Configure get file path and make a request for complete info
+                filedata = requests.get(config["getFile_url"].replace("<token>",token).replace("<file_path>", info["file_path"]))
+                
+                if "-local" in sys.argv:
+                    with open('example.jpg', 'wb') as f:
+                        f.write(filedata.content)
+            except:
+                print("Failed to get file data from Telegram")
+        else: 
+            print("Failed to get file path")
+
+if "-local" in sys.argv:
+    main(None)
+        
+
