26dc84a7cba778b8a7998c810a152dd8890de0eb
Lucas Magalhaes <lucas.magalhaes1985@gmail.com>
2020-11-07 16:01:13 -0300

    Add Message Handler from main

--- src/utils.py
+++ src/utils.py
@@ -129,7 +129,6 @@ class FloodFeel_Keyboards():
             return self.text, self.keyboard
 
 
-
 import hashlib
 def hashText(text,salt=None):
     """
@@ -141,6 +140,136 @@ def hashText(text,salt=None):
         return hashlib.sha256(salt.encode() + text.encode()).hexdigest()
     else:
         return hashlib.sha256(text.encode()).hexdigest()
+
+
+from datetime import datetime
+import time 
+date = datetime.today().strftime('%Y-%m-%d')
+timestamp_ms = round(time.time() * 1000)
+
+class MessageHandle():
+    def __init__(self,request_json,config):
+        self.location = None
+        self.photo_data = None
+        self.message_text = None
+        self.message = None
+        self.callback_data = None
+        self.data_to_bq = {}
+        self.data_type = None
+        self.hash_salt = config["hash_salt"]
+
+        try:
+            request_json["callback_query"]
+            self.is_callback_query = True
+        except: 
+            self.is_callback_query = False
+
+        self.bot_id = config["bot_id"]
+        self.bot_name = config["bot_name"]
+
+        # configure from request json
+        self._configure(request_json)
+
+        # check if request is valid
+        self._validate()
+
+        # configure data to be inserted on Big Query
+        self._configureBQData()
+
+    def get_reply_keyboard(self):
+        FFKeyboards = FloodFeel_Keyboards(callback_data=self.callback_data,message=self.message)
+        
+        return FFKeyboards.get_reply()
+
+    def _configure(self,request_json):
+        message = None
+
+        if self.is_callback_query == True:
+            message = request_json["callback_query"]["message"]
+            
+            try:
+                self.callback_data = request_json["callback_query"]["data"]
+            except:
+                self.callback_data = None
+                print("Fail to configure requested callback")
+        else: 
+            message = request_json["message"]
+            self.message = message
+            try:
+                self.message_text = request_json["message"]["text"]
+            except:
+                self.message_text = None
+
+        # chat info
+        try:
+            self.chat = message["chat"]
+        except:
+            print("Failed on extracting chat")
+            self.chat = None
+        
+        # request from info
+        try:
+            self.request_from = message["from"]
+        except:
+            print("Failed on extracting request from data")
+            self.request_from = None
+
+        # geolocation info
+        if "location" in message:
+            self.location = message["location"]
+
+        # photo info
+        if "photo" in message:
+            self.photo_data = message["photo"]
+        
+    def _validate(self):
+        if self.is_callback_query:
+            try:
+                if self.bot_id == self.request_from["id"] and self.bot_name == self.request_from["first_name"]:
+                    self.is_valid = True
+                else:
+                    self.is_valid = False
+                    print("Failed on validation: bot id doesnt match")
+            except:
+                print("Failed on callback query validation")
+        else:
+            if self.request_from["is_bot"] == True:
+                self.is_valid = False
+                print("Failed: request from bot")
+            else:
+                self.is_valid = True
+            
+    def get_user_id(self):
+        return self.chat["id"]
+    
+    def get_username(self):
+        return self.chat["first_name"]
+
+    def _configureBQData(self):
+        if self.location != None or self.photo_data != None:
+            self.data_to_bq["date"] = date
+            self.data_to_bq["user_id_hash"] = hashText(text=str(self.get_user_id()), salt=self.hash_salt)
+            self.data_to_bq["timestamp_ms"] = timestamp_ms
+
+            if self.location != None:
+                # table to send the data
+                self.data_type = "location"
+
+                self.data_to_bq["latitude"] = self.location["latitude"]
+                self.data_to_bq["longitude"] = self.location["longitude"]
+            elif self.photo_data != None:
+                # table to send the data
+                self.data_type = "photo"
+
+                # pick the best quality photo in the list (the last element is the best)
+                best_photo = self.photo_data[-1]
+
+                self.data_to_bq["file_id"] = best_photo["file_id"]
+                self.data_to_bq["file_unique_id"] = best_photo["file_unique_id"]
+    
+    def infos(self):
+        print(" **** INFOS ****\nchat_id:",self.chat["id"],"\nfirst_name:",self.chat["first_name"],"\nis_callback:",
+        self.callback_data != None,"\nis_location:",self.location != None,"\nis_photo:",self.photo_data != None)
 
 
 
