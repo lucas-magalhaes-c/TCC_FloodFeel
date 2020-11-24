a8eed361c8234b98b7d7716f727790ed236fe2bf
Lucas Magalhaes <lucas.magalhaes1985@gmail.com>
2020-11-23 22:17:50 -0300

    Start using big query handler module to query locations with flood

--- src/current_flood_locations/main.py
+++ src/current_flood_locations/main.py
@@ -1,14 +1,21 @@
 import requests
 import sys
 import json
-from utils import StorageHandler
+from utils import StorageHandler, BigQueryHandler
+from datetime import datetime
 
-lat_long_list = ["-23.545889,-46.733944","-23.508122801140846,-46.6896260551143","-23.5093793619657,-46.6658274385218","-23.516069505516327,-46.66750620864545","-23.513805260523746,-46.65334738595098","-23.516965771395327,-46.666784170819476","-23.455892102947654,-46.62799385849749","-23.52571074872409,-46.59556910991703","-23.52913300951429,-46.598004366258536","-23.528618281363837,-46.59687396568954"]
+date = datetime.today().strftime('%Y-%m-%d')
+
+# lat_long_list = ["-23.545889,-46.733944","-23.508122801140846,-46.6896260551143","-23.5093793619657,-46.6658274385218","-23.516069505516327,-46.66750620864545","-23.513805260523746,-46.65334738595098","-23.516965771395327,-46.666784170819476","-23.455892102947654,-46.62799385849749","-23.52571074872409,-46.59556910991703","-23.52913300951429,-46.598004366258536","-23.528618281363837,-46.59687396568954"]
 delimiter = "%7C"
 
 def main(request):
     config = json.loads(open("config_local.json").read()) if "-local" in sys.argv else json.loads(open("config.json").read())
     
+    # Query BQ to get all the current flood locations
+    BQH = BigQueryHandler(config=config)
+    lat_long_list = BQH.get_latlong(table_date=date)
+
     url_list = []
 
     # get url and insert token
@@ -56,13 +63,6 @@ def main(request):
         )
 
     print(f"Current flood locations per region refreshed: {len(url_list)} images")
-
-
-
-
-
-
-
 
 
 
